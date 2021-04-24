4c814fc1f92ec62cc234a811432b25b98942d173
Demon000 <demonsingur@gmail.com>
2021-04-24 10:44:13 +0000

    juice: inherit from base device configuration

---
+++ device.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
