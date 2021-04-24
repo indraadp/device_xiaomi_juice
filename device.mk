98d843af9f352666d1db3da355995bdbe6aa1569
Demon000 <demonsingur@gmail.com>
2021-04-24 11:02:37 +0000

    juice: Inherit from vendor if exists

--- device.mk
+++ device.mk
@@ -14,3 +14,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Inherit from vendor if exists
+$(call inherit-product-if-exists, vendor/xiaomi/juice/juice-vendor.mk)
