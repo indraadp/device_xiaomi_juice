ee191a6dfe93fba1ac40dc84dea0030938748e7c
Stallix <joey@evolution-x.org>
2021-04-25 05:25:24 +0000

    juice: add AospFrameworkRes overlay
    Co-authored-by: Stallix <joey@evolution-x.org>
    Signed-off-by: I-n-o-k <inok.dr189@gmail.com>

--- device.mk
+++ device.mk
@@ -47,3 +47,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/bluetooth_audio_policy_configuration.xml
+
+PRODUCT_PACKAGES += \
+    AospFrameworkResOverlay
