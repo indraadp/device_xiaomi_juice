8d4cde6068a1d2b63b49f3ad88e431a88c59d2c3
Rahul Krishna <Rahulkrishna585@gmail.com>
2021-04-28 01:31:28 +0000

    juice: Build audio packages and basic hidl hals

--- device.mk
+++ device.mk
@@ -42,6 +42,21 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
+    vendor.qti.hardware.audiohalext@1.0 \
+    vendor.qti.hardware.audiohalext-utils
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+PRODUCT_PACKAGES += \
+    tinymix \
+    libaudio-resampler
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
