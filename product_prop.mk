f7b021bb6cca02c348aa5ccfde0339272a372e11
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-02 21:25:20 +0700

    juice: Force triple frame buffers

--- product_prop.mk
+++ product_prop.mk
@@ -13,4 +13,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
