14e07e3e671404c33a358716cbc12ccb5ef3c090
I-n-o-k <75870644+I-n-o-k@users.noreply.github.com>
2021-05-02 21:25:20 +0700

    juice: Force triple frame buffers
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -17,4 +17,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
