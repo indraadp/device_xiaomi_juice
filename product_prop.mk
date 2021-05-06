8838459f2432b16ee6308a088ccef0fe221e7c77
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-05-07 07:59:56 +0800

    juice: unset some config Bluetooth
    Change Id: 8325f1e02504087844283d7467a39677d810264a

--- product_prop.mk
+++ product_prop.mk
@@ -1,15 +1,3 @@
-# Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.a2dp_offload.supported=false \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true \
-    vendor.audio.feature.a2dp_offload.enable=false \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=false
-
-# Bluetooth
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth.so 
-
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
