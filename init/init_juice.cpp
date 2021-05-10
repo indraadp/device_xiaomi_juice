9b8fe2298d143da6aee8974d1c4c20c6ca3d2899
aseelps <aseelpsathar@gmail.com>
2021-05-11 01:58:42 +0530

    juice: init: set  props based on  codename only
    Region not recognising
    
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- init/init_juice.cpp
+++ init/init_juice.cpp
@@ -71,27 +71,16 @@ void vendor_load_properties() {
         auto prop_name = "ro.product." + source + prop;
         property_override(prop_name.c_str(), value.c_str(), false);
     };
-
-    std::string region = GetProperty("ro.boot.hwc", "");
+    
     std::string hwname = GetProperty("ro.boot.product.hardware.sku", "");
 
     if (hwname == "lime") {
-            if (region == "CN" || region =="Japan") {
                 property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi 9 Power");
-                property_override("ro.product.device", "lime");
-            if (region == "India") {
-                property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi Note 9 4G");
-                property_override("ro.product.device", "lime");
-            if (region == "Global") {
-                property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi 9T");
-                property_override("ro.product.device", "lime");
-        }
+                property_override("ro.product.model", "Redmi 9T, Note 9 4G, 9 Power");
+                property_override("ro.product.device", "lime");     
     } else if (hwname == "lemon") {
         property_override("ro.product.brand", "Redmi");
-        property_override("ro.product.model", "Redmi 9T");
+        property_override("ro.product.model", "Redmi 9T NFC");
         property_override("ro.product.device", "lemon");
     } else if (hwname == "citrus") {
         property_override("ro.product.brand", "POCO");
