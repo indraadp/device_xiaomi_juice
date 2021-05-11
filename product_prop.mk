4cd201b4a808ed333f2c8981e9c3c22e2e92490f
Sushil Chauhan <sushilchauhan@codeaurora.org>
2021-05-11 07:30:33 +0000

    juice: Enable H/W composition
    Change Id: 7b3f4a87d351af70e11bd5847fb0f0f1aa937fdc

--- product_prop.mk
+++ product_prop.mk
@@ -13,6 +13,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
+    debug.sf.hw=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
     
