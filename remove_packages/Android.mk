75751cd71875bae29106de0f7566b3183e646126
I-n-o-k <inok.dr189@gmail.com>
2021-07-04 15:18:48 +0300

    juice: BringBack OPScreenRecord

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music RecorderPrebuilt SafetyHub PrebuiltCalculatorGoogle PrebuiltDeskClockGoogle PrebuiltCalendarGoogle CalculatorGooglePrebuilt CalendarGooglePrebuilt DeskClockGooglePrebuilt CalculatorGoogle CalendarGoogle DeskClockGoogle Calculator Calendar Deskclock Traceur
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music RecorderPrebuilt SafetyHub PrebuiltCalculatorGoogle PrebuiltDeskClockGoogle PrebuiltCalendarGoogle CalculatorGooglePrebuilt CalendarGooglePrebuilt DeskClockGooglePrebuilt CalculatorGoogle CalendarGoogle DeskClockGoogle Calculator Calendar Deskclock Traceur
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
