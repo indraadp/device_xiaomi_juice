f32b2312ac5db355d0f6b4128cf76ad222bd0041
I-n-o-k <75870644+I-n-o-k@users.noreply.github.com>
2021-07-08 11:41:39 +0700

    juice: add node for DT2W

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,9 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_juice
 TARGET_RECOVERY_DEVICE_MODULES := libinit_juice
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Power
 TARGET_USES_INTERACTION_BOOST := true
 
