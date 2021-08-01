0faaf1db99f625a42b74f553081e928e97fb195f
aseelps <aseelpsathar@gmail.com>
2021-08-01 09:42:49 +0530

    Revert "juice: Force triple frame buffers"
    This reverts commit 14e07e3e671404c33a358716cbc12ccb5ef3c090.
    
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -15,7 +15,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=1 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno
