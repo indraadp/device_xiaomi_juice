caccd42aef46629405499ade38e4acde7d4ea9b5
isnrdra <isanoorendra@gmail.com>
2021-08-06 00:04:32 +0000

    juice: PixelBlaster Bringup

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -4,21 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
 
-# Gapps
-#WITH_GAPPS := true
-#TARGET_GAPPS_ARCH := arm64
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nad_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
