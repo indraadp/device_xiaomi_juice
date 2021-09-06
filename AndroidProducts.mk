91680ec7b2cd1052a723c806be21a85beaf73d09
indraadp <indraproject777@gmail.com>
2021-09-06 05:59:03 +0000

    juice: bringup SuperiorOS
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/superior_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    superior_juice-user \
+    superior_juice-userdebug \
+    superior_juice-eng
