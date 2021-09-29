aecdf6b29bf5a1af2edbfc61dae2d3b37c83b03f
ANKIT KENE <keneankit01@gmail.com>
2021-09-29 07:39:57 +0000

    Ancient Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_juice.mk
+    $(LOCAL_DIR)/ancient_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_juice-user \
-    superior_juice-userdebug \
-    superior_juice-eng
+    ancient_juice-user \
+    ancient_juice-userdebug \
+    ancient_juice-eng
--- ancient_juice.mk
+++ ancient_juice.mk
@@ -11,19 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common superior stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common AncientOS stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
+#AncientOS
+ANCIENT_OFFICIAL := true
+FORCE_OTA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
 # GApps
-BUILD_WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_juice
+PRODUCT_NAME := ancient_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
