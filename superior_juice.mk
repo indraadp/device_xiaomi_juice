7af027e8f2b582ae7fa55595b768578a21e43275
Indra DP <53829264+indraadp@users.noreply.github.com>
2021-10-09 21:50:43 +0700

    juice: Update October Redfin fingerprint
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- superior_juice.mk
+++ superior_juice.mk
@@ -32,6 +32,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
-
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.211001.001 7641976 release-keys
