1c8bdd45d4ccc34da29850cd780c33205d3613c4
Indra DP <53829264+indraadp@users.noreply.github.com>
2022-03-04 13:51:32 +0700

    juice: Using GengKapak Clang to compile kernel
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,8 +107,9 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 #clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_VERSION := gengkapak
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
