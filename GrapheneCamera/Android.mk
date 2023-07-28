e38844dfc6fe5c8cad3bdbc9e2f53606d89bed46
indraadp <indraproject777@gmail.com>
2023-07-29 00:24:53 +0700

    juice: debloat some prebuilt apps from AEX
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- GrapheneCamera/Android.mk
+++ GrapheneCamera/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
 LOCAL_SRC_FILES := GrapheneCamera63.apk
-LOCAL_OVERRIDES_PACKAGES := Aperture SnapdragonCamera SnapdragonCamera2 Snap Camera2
+LOCAL_OVERRIDES_PACKAGES := Aperture SnapdragonCamera SnapdragonCamera2 Snap Camera2 GoogleCameraGo
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
